e53199b84b951d5c54b7423383c1215b08c1c5c2
Herik Rocha <herikrocha@gmail.com>
2017-10-04 23:56:25 -0300

    pass

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'herikrocha@gmail.com'
-EMAIL_HOST_PASSWORD = 'herikhr'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
