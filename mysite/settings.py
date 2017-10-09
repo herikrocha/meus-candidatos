23c0f0dfb478402e1ce17ec275982529ebfceef6
Herik Rocha <herikrocha@gmail.com>
2017-10-09 17:54:21 -0300

    email padrao

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'meuscandidatosapp@gmail.com'
+EMAIL_HOST_PASSWORD = 'meuscandidatos123'
 EMAIL_USE_TLS = True
 
 MIDDLEWARE_CLASSES = [
